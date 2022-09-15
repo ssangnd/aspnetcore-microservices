164cee2188dbc5b14839ab87dc5cc5cbe79c901e
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-09-15 23:30:00 +0700

    add configurations response

---
+++ src/BuildingBlocks/Shared/DTOs/Configurations/ApiResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs.Configurations
+{
+    public class ApiResult<T>
+    {
+        public ApiResult()
+        {
+        }
+
+        public ApiResult(bool isSucceeded, string message = null)
+        {
+            Message = message;
+            IsSucceeded = isSucceeded;
+        }
+
+        public ApiResult(bool isSucceeded, T data, string message = null)
+        {
+            Data = data;
+            Message = message;
+            IsSucceeded = isSucceeded;
+        }
+
+        public bool IsSucceeded { get; set; }
+        public string Message { get; set; }
+        public T Data { get; }
+    }
+}
---
+++ src/BuildingBlocks/Shared/DTOs/Configurations/RequestParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs.Configurations
+{
+    public class RequestParameters
+    {
+        public string OrderBy { get; set; }
+
+        public string SearchTerm { get; set; }
+    }
+}
